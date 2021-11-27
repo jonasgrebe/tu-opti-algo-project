1d0e82913a52ab5efdd2ca655560a18909a6c283
MaggiR <mark.rothermel@web.de>
2021-11-27 20:41:02 +0100

    Make OptProblem interface more intuitive

--- problems/optimization.py
+++ problems/optimization.py
@@ -4,15 +4,21 @@ from abc import abstractmethod
 
 class OptProblem(AbstractClass):
 
-    def __init__(self, f, is_max):
-        self.f = f
+    def __init__(self, is_max):
         self.is_max = is_max
 
-        self.f = 3 * f
+    @abstractmethod
+    def f(self, x):
+        """Objective function of this optimization problem.
+
+        :param x: the solution to evaluate
+        :return: the value of x
+        """
+        raise NotImplementedError
 
     @abstractmethod
     def is_feasible(self, x):
-        """Returns whether x is a feasible solution or not
+        """Returns whether x is a feasible solution or not.
 
         :param x: the solution to check
         :return: True if x is feasible else False
