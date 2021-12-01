e98e8f451d355777ff260d501304a0ecb3440cf1
Jonas Grebe <26788162+jonasgrebe@users.noreply.github.com>
2021-12-01 15:27:34 +0100

    Update local_search.py

--- algos/local_search.py
+++ algos/local_search.py
@@ -7,32 +7,37 @@ import time
 def local_search(problem: NeighborhoodProblem, gui: RectanglePackingGUI):
     # Step 1: Start with an arbitrary feasible solution
     current_solution = problem.get_arbitrary_solution()
-
     assert problem.is_feasible(current_solution)
+    current_solution_value = problem.f(current_solution)
+
+    gui.set_current_solution(current_solution)
 
     # Step 2: While there is a better solution nearby, go to that solution
     while True:
         neighborhood = problem.get_neighborhood(current_solution)
-        print("neighborhood size:", len(neighborhood))
 
         neighborhood_values = [problem.f(neighbor_solution) for neighbor_solution in neighborhood]
 
         # Degree of freedom: use best neighbor
         if problem.is_max:
             best_neighbor_idx = np.argmax(neighborhood_values)
-            is_better = neighborhood_values[best_neighbor_idx] > problem.f(current_solution)
+            is_better = neighborhood_values[best_neighbor_idx] > current_solution_value
         else:
             best_neighbor_idx = np.argmin(neighborhood_values)
-            is_better = neighborhood_values[best_neighbor_idx] < problem.f(current_solution)
+            is_better = neighborhood_values[best_neighbor_idx] < current_solution_value
 
         if not is_better:
             break
 
         current_solution = neighborhood[best_neighbor_idx]
+        current_solution_value = neighborhood_values[best_neighbor_idx]
+
         # print("current_solution:", current_solution)
-        print("current value:", problem.f(current_solution))
+        print("neighborhood size:", len(neighborhood))
+        print("current value:", current_solution_value)
+
         gui.set_current_solution(current_solution)
-        time.sleep(8)
+        time.sleep(2)
 
     # Step 3: deliver final solution (local optimum)
     return current_solution
