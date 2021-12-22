c8b68afe9f0b46fe58c08d1b2832eb369700289e
MaggiR <mark.rothermel@web.de>
2021-12-22 22:14:49 +0100

    Amend run parameters

--- run_algo.py
+++ run_algo.py
@@ -3,7 +3,7 @@ from problems.rectangle_packing.problem import RectanglePackingProblemGeometryBa
 
 
 # For performance test
-problem = RectanglePackingProblemGeometryBased(8, 1000, 1, 8, 1, 8)
+problem = RectanglePackingProblemGeometryBased(8, 1000, 1, 7, 1, 7)
 init_sol = problem.get_arbitrary_solution()
 solution = local_search(init_sol, problem)
 print("objective value:", problem.objective_function(solution))
