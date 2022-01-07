8c0075cf33f3bd7a7b1ecaf6e70f2f49fce55ee4
Jonas Grebe <26788162+jonasgrebe@users.noreply.github.com>
2022-01-07 18:34:31 +0100

    Update run_algo.py

--- run_algo.py
+++ run_algo.py
@@ -1,10 +1,10 @@
 from algos.local import local_search
 from algos.greedy import greedy_search
 from problems.rectangle_packing.problem import RectanglePackingProblemGeometryBased
-from problems.rectangle_packing.problem import RectanglePackingProblemGreedyLargestAreaStrategy
+from problems.rectangle_packing.problem import RectanglePackingProblemGreedyStrategy
 
 
 # For performance test
-problem = RectanglePackingProblemGreedyLargestAreaStrategy(8, 100, 1, 7, 1, 7)
+problem = RectanglePackingProblemGreedyStrategy(8, 100, 1, 7, 1, 7)
 solution = greedy_search(problem)
 print("objective value:", problem.objective_function(solution))
